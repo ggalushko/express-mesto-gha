036687c26ee8572d04145269c057f353f92d7880
ggalushko <titanmelchior@gmail.com>
2023-07-30 13:55:41 +0300

    move method

--- routes/users.js
+++ routes/users.js
@@ -11,12 +11,6 @@ const {
 usersRouter.get('/', getUsers);
 usersRouter.get('/me', getCurrentUser);
 
-usersRouter.get('/:userId', celebrate({
-  params: Joi.object().keys({
-    userId: Joi.string().length(24).hex().required(),
-  }),
-}), getUser);
-
 usersRouter.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
@@ -30,5 +24,11 @@ usersRouter.patch('/me/avatar', celebrate({
     avatar: Joi.string().required().pattern(/https?:\/\/(www\.)?[-\w@:%\.\+~#=]{1,256}\.[a-z0-9()]{1,6}\b([-\w()@:%\.\+~#=//?&]*)/i),
   }),
 }), updateUserAvatar);
+
+usersRouter.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex().required(),
+  }),
+}), getUser);
 
 module.exports = usersRouter;
